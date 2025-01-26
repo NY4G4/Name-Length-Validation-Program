221101cbe8f404b1a6308e976130cd9a81d9454a
NY4G4 <nyagaclive@gmail.com>
2025-01-26 16:15:52 +0300

    added comments to the program

--- car_simulation_game.py
+++ car_simulation_game.py
@@ -1,20 +1,20 @@
-command = ""
-car_started = False
+command = "" # stores the commands entered by the user
+car_started = False # describes the state of the car (whether it is started or not)
 
 while True:
     command = input("> ").lower()
     if command == "start":
-        if car_started:
+        if car_started: # checks if the car has already started
             print("The car has already started!")
         else:
-            car_started = True
+            car_started = True # starts the car
             print("The car is starting...")
             print("The car has started.")
     elif command == "stop":
-        if not car_started:
+        if not car_started: # checks if the car has already stopped
             print("The car has already stopped!")
         else:
-            car_started = False
+            car_started = False # stops the car
             print("The car is stopping...")
             print("The car has stopped.")
     elif command == "help":
